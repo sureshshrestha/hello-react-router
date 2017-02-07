14850cd63588a7321ddabe6b2135f7538879d6b6
Suresh Shrestha <surestha@outlook.com>
2017-02-07 18:06:23 +0545

    10. React Router: Redirect.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Redirect, Link, hashHistory } from 'react-router';
 
-const Page = (props) => 
-	<div><h1>{props.location.query.message || 'Hello'}</h1><Links /></div>
+const Home = () => <div><h1>Home</h1><Links /></div>;
+const About = () => <div><h1>About</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
 
-const Links = () =>
-	<nav>
-		<Link to={ {pathname: '/', query: {message: 'Yo' }}}>Yo</Link>
-	</nav>
+const Links = () => {
+	return (
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="about-us">About Us</Link>
+			<Link to="about">About</Link>
+			<Link to="contact">Contact</Link>
+		</nav>
+	)
+};
 
-class App extends React.Component {
-	render() {
-		return (
-			<Router history={ hashHistory }>
-				<Route path="/" component={Page}></Route>
-			</Router>
-		)
-	}
-}
+const App = () => {
+	return (
+		<Router history={ hashHistory }>
+			<Route path="/" component={Home}></Route>
+			<Route path="/about-us" component={About}></Route>
+			<Route path="/contact" component={Contact}></Route>
+			<Redirect from='/about' to='/about-us'></Redirect>
+		</Router>
+	)
+};
 
 export default App;
