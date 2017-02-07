2502252f4fae246e6b1814599c0b968a0a78d14c
Suresh Shrestha <surestha@outlook.com>
2017-02-07 17:00:05 +0545

    4 React router activeStyle and activeClassName.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const About = () => <div><h1>About</h1><Links /></div>;
 const Contact = () => <div><h1>Contact</h1><Links /></div>;
 const Links = () =>
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
-      <Link to="contact">Contact</Link>
+      <Link activeStyle={{color: 'green'}} to="/">Home</Link>
+      <Link activeStyle={{color: 'green'}} to="about">About</Link>
+      <Link activeClassName="active" to="contact">Contact</Link>
     </nav>
 
 
