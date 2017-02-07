0073036cb67d7aeb48ad7192f564bf093d488e1e
Suresh Shrestha <surestha@outlook.com>
2017-02-07 18:13:50 +0545

    11. React Router: setRouteWillLeaveHook.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Router, Route, Redirect, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, hashHistory } from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
+// const Home = () => <div><h1>Home</h1><Links /></div>;
+
+class Home extends React.Component {
+	componentWillMount() {
+		this.context.router.setRouteLeaveHook(
+			this.props.route,
+			this.routerWillLeave
+		)
+	}
+
+	routerWillLeave( nextLocation ){
+		console.log (`leaveing home for ${nextLocation.pathname}`)
+	}
+
+	render() {
+		return <div><h1>Home</h1><Links /></div>;
+	}
+}
+
+Home.contextTypes = { router: React.PropTypes.object.isRequired }
+
 const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
 
 const Links = () => {
 	return (
 		<nav>
 			<Link to="/">Home</Link>
-			<Link to="about-us">About Us</Link>
 			<Link to="about">About</Link>
-			<Link to="contact">Contact</Link>
 		</nav>
 	)
 };
@@ -20,9 +37,7 @@ const App = () => {
 	return (
 		<Router history={ hashHistory }>
 			<Route path="/" component={Home}></Route>
-			<Route path="/about-us" component={About}></Route>
-			<Route path="/contact" component={Contact}></Route>
-			<Redirect from='/about' to='/about-us'></Redirect>
+			<Route path="/about" component={About}></Route>
 		</Router>
 	)
 };
