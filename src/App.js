8a04dede0060ca981116ee76298858348032b10e
Suresh Shrestha <surestha@outlook.com>
2017-02-07 17:08:02 +0545

    5 React Router Nested Routes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory } from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
+const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
+const About = (props) => <div><h1>About</h1><Links />{props.children}</div>;
 const Contact = () => <div><h1>Contact</h1><Links /></div>;
 const Links = () =>
     <nav>
-      <Link activeStyle={{color: 'green'}} to="/">Home</Link>
-      <Link activeStyle={{color: 'green'}} to="about">About</Link>
-      <Link activeClassName="active" to="contact">Contact</Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/about/contact">Contact</Link>
     </nav>
 
 
 const App = () => {
   return (
     <Router history={ hashHistory }>
-      <Route path="/" component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/contact" component={Contact}></Route>
+      <Route path="/" component={Home}>
+      	<Route path="about" component={About}>
+      		<Route path="contact" component={Contact}></Route>
+      	</Route>
+      </Route>
     </Router>
   )
 };
